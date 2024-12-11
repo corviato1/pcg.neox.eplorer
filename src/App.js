32a508cc6b8a6d48d481974e208052e33f71de32
corviato1 <44780076+corviato1@users.noreply.github.com>
2024-12-10 20:24:14 -0800

    reset back

--- src/App.js
+++ src/App.js
@@ -1,48 +1,23 @@
-import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import About from "./components/About";
-import Highlights from "./components/Highlights";
-import Contact from "./components/Contact";
-import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      {/* Landing Page */}
-      <div className="landing-page">
-        {/* Cards */}
-        <section className="cards">
-          <div className="card" id="card-one">
-            One
-          </div>
-          <div className="card" id="card-two">
-            Two
-          </div>
-          <div className="card" id="card-three">
-            Three
-          </div>
-        </section>
-
-        {/* About */}
-        <section className="about">
-          <About />
-        </section>
-
-        {/* Highlights */}
-        <section className="highlights">
-          <Highlights />
-        </section>
-
-        {/* Contact */}
-        <section className="contact">
-          <Contact />
-        </section>
-
-        {/* Login */}
-        <section className="login">
-          <Login />
-        </section>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
