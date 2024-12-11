2ec1103f9164e1787543bf372d3c81bf2d094332
corviato1 <44780076+corviato1@users.noreply.github.com>
2024-12-11 10:25:57 -0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./About.css";
 
 const About = () => (
-  <div>
-    <h1 style={{ fontSize: "2.58em" }}>Title</h1>
-    <p style={{ fontSize: "1em" }}>One sentence explainer</p>
+  <div className="about-body">
+    <h1 style={{ fontSize: "2.58em" }}>Real World Assets traded on web3 </h1>
+    <p style={{ fontSize: "1em" }}>
+      PCG Slabs rated 9, 9.5, and 10s minted on Neoxa
+    </p>
     <blockquote style={{ fontSize: "0.42em" }}>“This is the quote.”</blockquote>
   </div>
 );
