b00e1486b1b01a2f46eb11a053b174c5316b9bd8
corviato1 <44780076+corviato1@users.noreply.github.com>
2024-12-10 21:08:57 -0800

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,34 +1,50 @@
 import React from "react";
 
 const Contact = () => (
-  <div>
+  <div className="contact-container">
     <a
       href="https://ipfs.io/ipfs/QmeySH9R9wDHfNfVGhVYNJTqBLBC3eJQENLuwx2UdApW6J"
       target="_blank"
       rel="noopener noreferrer"
     >
-      Exchange
+      <img
+        src={`${process.env.PUBLIC_URL}/img/exchange.png`}
+        alt="Exchange"
+        className="contact-image"
+      />
     </a>
     <a
       href="https://ipfs.io/ipfs/QmYpqRZeGFLUTrrNzjW5LdUjHgFWonvmjjhaxyWxLd6CKF"
       target="_blank"
       rel="noopener noreferrer"
     >
-      Neoxa
+      <img
+        src={`${process.env.PUBLIC_URL}/img/neoxa.png`}
+        alt="Neoxa"
+        className="contact-image"
+      />
     </a>
     <a
       href="https://ipfs.io/ipfs/QmS3W2FwFPv444ftyEp6xJUjAXRNuzELtcGPz2UzZe7Zpe"
       target="_blank"
       rel="noopener noreferrer"
     >
-      Premier Card Grading
+      <img
+        src={`${process.env.PUBLIC_URL}/img/pcg.png`}
+        alt="Premier Card Grading"
+        className="contact-image"
+      />
     </a>
     <a
       href="https://ipfs.io/ipfs/QmZMwUZka3ssmU3BgFBvB257SEjAR37qD8oCRGgMfPGsJk"
       target="_blank"
       rel="noopener noreferrer"
     >
-      Trader
+      <img
+        src={`${process.env.PUBLIC_URL}/img/trader.png`}
+        alt="Trader"
+        className="contact-image"
+      />
     </a>
   </div>
 );
