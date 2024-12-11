dca0a0063c520c23c48166e7bd3023a56195ee26
corviato1 <44780076+corviato1@users.noreply.github.com>
2024-12-10 20:15:04 -0800

    Create Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Contact = () => (
+  <div>
+    <a
+      href="https://ipfs.io/ipfs/QmeySH9R9wDHfNfVGhVYNJTqBLBC3eJQENLuwx2UdApW6J"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Exchange
+    </a>
+    <a
+      href="https://ipfs.io/ipfs/QmYpqRZeGFLUTrrNzjW5LdUjHgFWonvmjjhaxyWxLd6CKF"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Neoxa
+    </a>
+    <a
+      href="https://ipfs.io/ipfs/QmS3W2FwFPv444ftyEp6xJUjAXRNuzELtcGPz2UzZe7Zpe"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Premier Card Grading
+    </a>
+    <a
+      href="https://ipfs.io/ipfs/QmZMwUZka3ssmU3BgFBvB257SEjAR37qD8oCRGgMfPGsJk"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Trader
+    </a>
+  </div>
+);
+
+export default Contact;
