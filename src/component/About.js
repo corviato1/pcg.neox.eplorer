f31c455264cfb0c2ddac0637ec4ca8a30e8f086c
corviato1 <44780076+corviato1@users.noreply.github.com>
2024-12-10 20:11:43 -0800

    About.js

---
+++ src/component/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <h1 style={{ fontSize: "2.58em" }}>Title</h1>
+    <p style={{ fontSize: "1em" }}>One sentence explainer</p>
+    <blockquote style={{ fontSize: "0.42em" }}>“This is the quote.”</blockquote>
+  </div>
+);
+
+export default About;
