57efec035472293678f545c920c0d130a44b337a
corviato1 <44780076+corviato1@users.noreply.github.com>
2024-12-11 15:12:35 -0800

    Update cardPool.js

--- src/data/cardPool.js
+++ src/data/cardPool.js
@@ -1,37 +1,54 @@
+// Pool of all available graded cards
 export const cardsPool = [
   {
     id: 1,
-    title: "MTG Black Lotus",
-    image: "/img/cards/black-lotus.jpg",
+    title:
+      "Dark Magician Girl - The Dark Side of Dimensions Movie Pack Gold Edition",
+    image:
+      "/img/all-slabs/Dark Magician Girl - The Dark Side of Dimensions Movie Pack Gold Edition (MVP1-ENG).jpg",
   },
   {
     id: 2,
-    title: "Pokemon Charizard",
-    image: "/img/cards/charizard.jpg",
+    title: "Dark Magician Girl (2020) - The Lost Art Promotion",
+    image:
+      "/img/all-slabs/Dark Magician Girl (2020) - The Lost Art Promotion (LART).jpg",
   },
   {
     id: 3,
-    title: "Yu-Gi-Oh! Blue Eyes White Dragon",
-    image: "/img/cards/blue-eyes.jpg",
+    title:
+      "Dark Magician Girl (Ghost Rare) - Ghosts From the Past The 2nd Haunting",
+    image:
+      "/img/all-slabs/Dark Magician Girl (Ghost Rare) - Ghosts From the Past The 2nd Haunting (GFP2).jpg",
   },
   {
     id: 4,
-    title: "MTG Time Walk",
-    image: "/img/cards/time-walk.jpg",
+    title:
+      "Dark Magician Girl the Dragon Knight (Alternate Art) (Blue) - Dragons of Legend The Complete Series",
+    image:
+      "/img/all-slabs/Dark Magician Girl the Dragon Knight (Alternate Art) (Blue) - Dragons of Legend The Complete Series (DLCS).jpg",
   },
   {
     id: 5,
-    title: "Pokemon Pikachu Illustrator",
-    image: "/img/cards/pikachu-illustrator.jpg",
+    title:
+      "Dark Magician Girl the Dragon Knight (Quarter Century Secret Rare) - Quarter Century Bonanza",
+    image:
+      "/img/all-slabs/Dark Magician Girl the Dragon Knight (Quarter Century Secret Rare) - Quarter Century Bonanza.jpg",
   },
   {
     id: 6,
-    title: "Yu-Gi-Oh! Dark Magician",
-    image: "/img/cards/dark-magician.jpg",
+    title: "Jeweled Lotus (Extended Art) - Commander Legends",
+    image:
+      "/img/all-slabs/Jeweled Lotus (Extended Art) - Commander Legends (CMR).jpg",
   },
-  // Add more cards as needed
+  {
+    id: 7,
+    title: "Pikachu ex - 238/191 - SV08 Surging Sparks",
+    image:
+      "/img/all-slabs/Pikachu ex - 238191 - SV08 Surging Sparks (SV08).jpg",
+  },
 ];
 
+// Function to get a subset of featured cards
 export const getFeaturedCards = (count) => {
   // Shuffle and return a subset of cards
   const shuffled = [...cardsPool].sort(() => 0.5 - Math.random());
